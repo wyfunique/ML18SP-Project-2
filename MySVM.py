1522d5e6765d8782b14d33658c8bf120909d781c
wyfunique <yifanwang2017@gmail.com>
2018-04-20 11:14:44 -0400

    Implemented probability output.

--- MySVM.py
+++ MySVM.py
@@ -1,10 +1,22 @@
 import scipy.io as sio
 import numpy as np
+import sklearn
 from sklearn.model_selection import KFold
 from sklearn.svm import SVC
 from sklearn.grid_search import GridSearchCV
+from sklearn.preprocessing import normalize
 
-
+def GetClassLabels(svm_predict_prob):
+    
+    ClassLabels = np.zeros((svm_predict_prob.shape[0], svm_predict_prob.shape[1]+1))
+    for i in range(svm_predict_prob.shape[0]):
+        ClassLabels[i, :-1] = svm_predict_prob[i]
+        sorted_prob = np.sort(svm_predict_prob[i])[::-1]
+        interval = np.subtract(sorted_prob[:-1], sorted_prob[1:])
+        ClassLabels[i, -1] = 1.0 - interval[0] / np.sum(interval)
+    ClassLabels = normalize(ClassLabels, axis=1)
+    return ClassLabels
+    
 def TrainMySVM(XEstimate, XValidate, ClassLabelsEstimate, ClassLabelsValidate, Parameters=None):
     # Each label in ClassLabelsEstimate and ClassLabelsValidate is 5 dimensional vector like [1,-1,-1,-1,-1];
     # So ClassLabelsEstimate and ClassLabelsValidate are both N-by-5 numpy arrays.
@@ -14,20 +26,21 @@ def TrainMySVM(XEstimate, XValidate, ClassLabelsEstimate, ClassLabelsValidate, P
         train_labels[i] = np.where(ClassLabelsEstimate[i] == 1)[0]
     for i in range(ClassLabelsValidate.shape[0]):
         cv_labels[i] = np.where(ClassLabelsValidate[i] == 1)[0]
-      
+    
     C_range = 10. ** np.arange(-2, 3) # 10 is best
     gamma_range = 10. ** np.arange(-2, 3) # 1 is best
     param_grid = dict(gamma=gamma_range, C=C_range)
-    grid = GridSearchCV(SVC(max_iter=100, decision_function_shape='ovo', verbose=True), param_grid=param_grid, cv=5)
+    grid = GridSearchCV(SVC(max_iter=100, decision_function_shape='ovo', probability=True, verbose=True), param_grid=param_grid, cv=5)
     grid.fit(XEstimate, train_labels)
     svm = grid.best_estimator_
-    #svm = SVC(C=10, gamma=1, max_iter=100, decision_function_shape='ovo', verbose=True)
+    #svm = SVC(C=10, gamma=1, max_iter=100, decision_function_shape='ovo', probability=True, verbose=True)
     svm.fit(XEstimate, train_labels)
-    cv_predict = svm.predict(XValidate)
-    cv_acc = sum([1 for i in range(cv_labels.shape[0]) if cv_predict[i] == cv_labels[i]]) / float(cv_labels.shape[0])
+    cv_predict_prob = svm.predict_proba(XValidate)
+    #cv_predict = svm.predict(XValidate)
+    #cv_acc = sum([1 for i in range(cv_labels.shape[0]) if cv_predict[i] == cv_labels[i]]) / float(cv_labels.shape[0])
     #print svm.score(XValidate, cv_labels)
-    print "==== CV Accuracy: %f ===="%cv_acc
-    Yvalidate = cv_predict
+    #print "==== CV Accuracy: %f ===="%cv_acc
+    Yvalidate = GetClassLabels(cv_predict_prob)
     EstParameters = {}
     InternalParams = ['support_', 'support_vectors_', 'n_support_', 'dual_coef_', 'coef_', 
     'intercept_', '_sparse', 'shape_fit_', '_dual_coef_', '_intercept_', 'probA_', 'probB_', '_gamma', 'classes_']
@@ -44,7 +57,7 @@ def TrainMySVM(XEstimate, XValidate, ClassLabelsEstimate, ClassLabelsValidate, P
 def TestMySVM(XTest, EstParameters, Parameters=None):
     hyperParams = EstParameters['HyperParameters']
     trainedParams = EstParameters["EstimatedParameters"]
-    svm = SVC(C=hyperParams['C'], gamma=hyperParams['gamma'], kernel=hyperParams['kernel'], max_iter=hyperParams['max_iter'], decision_function_shape='ovo')
+    svm = SVC(C=hyperParams['C'], gamma=hyperParams['gamma'], kernel=hyperParams['kernel'], max_iter=hyperParams['max_iter'], decision_function_shape='ovo', probability=True)
     InternalParams = ['support_', 'support_vectors_', 'n_support_', 'dual_coef_', 'coef_', 
     'intercept_', '_sparse', 'shape_fit_', '_dual_coef_', '_intercept_', 'probA_', 'probB_', '_gamma', 'classes_']
     for p in InternalParams:
@@ -52,6 +65,6 @@ def TestMySVM(XTest, EstParameters, Parameters=None):
             exec('svm.%s = trainedParams[p]'%p) 
         except: 
             continue
-    Ytest = svm.predict(XTest)
+    test_predict_prob = svm.predict_proba(XTest)
+    Ytest = GetClassLabels(test_predict_prob)
     return Ytest
-    
